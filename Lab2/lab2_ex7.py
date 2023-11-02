c8cc75d3fa4e08c15c5d8506eebe7a631b7fac7a
xSuly <81815152+xSuly@users.noreply.github.com>
2023-11-02 19:37:35 +0200

    reupload lab2_ex7 pe baza intrebarilor de la lab4

--- Lab2/lab2_ex7.py
+++ Lab2/lab2_ex7.py
@@ -11,15 +11,20 @@ t = np.linspace(0, 1, f0)
 
 sinusoid_A = np.sin(2*pi*f0*t)
 
-t_decimat = np.linspace(0, 1, int(f0/4))
-#t_decimat = t_decimat[1::4]
+sinusoid_A_decimat = sinusoid_A[::4]
+f_decimat = int(f0/4)
+t_decimat = np.linspace(0, 1, f_decimat)
 
-sinusoid_B = np.sin(2*pi*t_decimat*f0)
+#sinusoid_B = np.sin(2*pi*t_decimat*f0)
 
-fig, axs = plt.subplots(2)
+sinusoid_A_2start = sinusoid_A[1::4]
+
+fig, axs = plt.subplots(3)
 fig.suptitle('Semnalele')
-axs[0].plot(t, sinusoid_A)
-axs[1].plot(t_decimat, sinusoid_B)
+axs[0].stem(t, sinusoid_A)
+axs[1].stem(t_decimat, sinusoid_A_decimat)
+axs[2].stem(t_decimat, sinusoid_A_2start)
 
 plt.show()
 #E rezolvarea fara subpct B, nu inteleg exact cerinta, am incercat prin t_decimat[1::4] insa nu stiu cat de corect este
+#Corectie dupa intrebarile de la inceputul LAB4 -> finish a, adaugat subpct b
