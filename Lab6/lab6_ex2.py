ddd8aac7c9e5235b6b179c47e18c76452756b71f
xSuly <81815152+xSuly@users.noreply.github.com>
2023-11-22 01:04:29 +0200

    lab6 ex2 reupload

--- Lab6/lab6_ex2.py
+++ Lab6/lab6_ex2.py
@@ -10,12 +10,10 @@ valoare = 5
 p = np.random.randint(-valoare, valoare, N + 1)
 q = np.random.randint(-valoare, valoare, N + 1)
 
-print(p)
-print(q)
+#print(p)
+#print(q)
 r = np.convolve(p, q)
-r2 = np.fft.fft(np.fft.fft(p) * np.fft.fft(q))
-
-
+r2 = np.real(np.fft.ifft(np.fft.fft(p, n=2*N+1) * np.fft.fft(q, n=2*N+1))) #folosim 2*N+1 pentru ca dupa ce inmultim 2 polinoame atatia coeficienti vom avea in noul polinom si trebuie sa umplem cu 0 pana la 2*N+1 sa nu se piarda valori
 
 print(r)
 
